68df5634e977268ce34d9046ae6219b8e3e8a64b
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-01-09 21:06:00 +0600

    Dummy GetVillas implementation complete

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using STMSApi.Models;
 
 namespace STMSApi.Controllers
 {
+    [Route("api/VillaApi")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+                new Villa { Id = 1, Name = "Pool View"},
+                new Villa { Id = 2, Name = "Alif Mansion"}
+            };
+        }
     }
 }
